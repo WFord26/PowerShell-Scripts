716cdbc2d8f123a8aa18d91b326cef8dd52708e7
SanchosPoncho <wford@managedsolution.com>
2025-04-14 11:02:42 -0600

    Enhance Get-WWWSSLInfo function to support verbose logging and enforce TLS 1.2 for secure connections

--- www/Get-WWWSSLInfo.ps1
+++ www/Get-WWWSSLInfo.ps1
@@ -10,16 +10,21 @@ function Get-WWWSSLInfo {
     try {
         # Check if URL has https://
         if ($url -notmatch "^https://") {
-            $url = "https://$url"
+            $url = "https://$($url)"
         }
         
+        Write-Verbose "URL: $url"
+
         # If port is specified, append it to the URL
         if ($port) {
             $url = "$($url):$($port)"
+            Write-Verbose "URL with port: $url"
         }
         
         $request = [Net.HttpWebRequest]::Create($url)
         $request.ServerCertificateValidationCallback = { $true }
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
         $request.GetResponse() | Out-Null
         $cert = $request.ServicePoint.Certificate
         $cert2 = New-Object Security.Cryptography.X509Certificates.X509Certificate2 $cert
